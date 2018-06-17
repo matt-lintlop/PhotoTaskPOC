3bd909069847f94bfa20ff918bfdd8d3dbf09989
Matt Lintlop <matt_lintlop@icloud.com>
2018-06-16 19:04:17 -0700

    Initial Commit

---
+++ PhotoTaskPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoTaskPOC
+//
+//  Created by Matt Lintlop on 6/16/18.
+//  Copyright © 2018 Matt Lintlop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
