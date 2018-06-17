57190b28826df6ae220735e7045a55bc9b7fcb40
Matt Lintlop <matt_lintlop@icloud.com>
2018-06-16 19:34:48 -0700

    Added PhotoTaskViewController.

---
+++ PhotoTaskPOC/PhotoTaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoTaskViewController.swift
+//  PhotoTaskPOC
+//
+//  Created by Matt Lintlop on 6/16/18.
+//  Copyright © 2018 Matt Lintlop. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Loaded PhotoTaskViewController")
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
