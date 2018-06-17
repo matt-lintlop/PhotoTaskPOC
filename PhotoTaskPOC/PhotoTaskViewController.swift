676c42932b8c4d183027dc43ac6a5d9f5fd14963
Matt Lintlop <matt_lintlop@icloud.com>
2018-06-16 20:37:42 -0700

    Added outlet for mainHeadingLabel.

--- PhotoTaskPOC/PhotoTaskViewController.swift
+++ PhotoTaskPOC/PhotoTaskViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class PhotoTaskViewController: UIViewController {
 
+    @IBOutlet weak var mainHeadingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
