c892617c9ed8213302abdfb8213236bb2a9a7ff3
Matt Lintlop <mac2matt@yahoo.com>
2018-06-30 13:35:04 -0700

    Added UIViewFromNib.swift

---
+++ PhotoTaskPOC/UIViewFromNib.swift
@@ -0,0 +1,39 @@
+//
+//  UIViewFromNib.swift
+//  Pegasus
+//
+//  Created by Matt Lintlop on 6/30/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class UIViewFromNib: UIView {
+    
+    var contentView: UIView!
+    
+    var nibName: String {
+        return String(describing: type(of: self))
+    }
+    
+    //MARK:
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        loadViewFromNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        loadViewFromNib()
+    }
+    
+    //MARK:
+    func loadViewFromNib() {
+        contentView = Bundle.main.loadNibNamed(nibName, owner: self, options: nil)?[0] as! UIView
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        contentView.frame = bounds
+        addSubview(contentView)
+    }
+}
