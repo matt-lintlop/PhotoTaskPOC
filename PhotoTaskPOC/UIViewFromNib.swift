bf1bc5c9084e15ab52da5176d7838e01650c1a66
Matt Lintlop <mac2matt@yahoo.com>
2018-07-01 05:39:13 -0700

    Added UIView extension to load the view from a XIB file.

--- PhotoTaskPOC/UIViewFromNib.swift
+++ PhotoTaskPOC/UIViewFromNib.swift
@@ -37,3 +37,38 @@ class UIViewFromNib: UIView {
         addSubview(contentView)
     }
 }
+
+public extension UIView {
+    public class func fromNib<T : UIView>(nibNameOrNil: String? = nil, type: T.Type) -> T? {
+        var view: T?
+        let name: String
+        if let nibName = nibNameOrNil {
+            name = nibName
+        } else {
+            // Most nibs are demangled by practice, if not, just declare string explicitly
+            name = nibName
+        }
+        guard let nibViews = Bundle.main.loadNibNamed(name, owner: nil, options: nil) else {
+            return nil
+        }
+        for v in nibViews {
+            if let tog = v as? T {
+                view = tog
+            }
+        }
+        return view
+    }
+    
+    public class var nibName: String {
+        let name = "\(self)".components(separatedBy: ".").first ?? ""
+        return name
+    }
+    
+    public class var nib: UINib? {
+        if let _ = Bundle.main.path(forResource: nibName, ofType: "nib") {
+            return UINib(nibName: nibName, bundle: nil)
+        } else {
+            return nil
+        }
+    }
+}
